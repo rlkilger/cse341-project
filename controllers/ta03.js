20f6c1df3852c6470f77e504a9e527312f36178d
rlkilger <rlkilger@gmail.com>
2021-09-28 19:26:42 -0600

    ta03 initial

--- controllers/ta03.js
+++ controllers/ta03.js
@@ -1,13 +1,3 @@
-//TA03 PLACEHOLDER
-const express = require('express');
-const router = express.Router();
-const ta03Controller = require('../controllers/ta03.js');
-
-router.get('/', ta03Controller.getProducts);
-router.get('/search', ta03Controller.getSearchProducts);
-
-module.exports = router;
-
 // TA03 controller
 
 const Product = require('../models/product');
